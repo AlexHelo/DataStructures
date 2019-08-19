be602856827ac3545e4780ad91bdd9a014dab110
GinaJames <gina10111@hotmail.com>
2019-08-19 11:06:54 -0500

    Added Circle Classes

--- ShapesHW/src/Shapes/Objects2D/circle.java
+++ ShapesHW/src/Shapes/Objects2D/circle.java
@@ -1,24 +1,33 @@
 package Shapes.Objects2D;
 
 import Shapes.Shape2D;
+import java.lang.Math;
 
 /**
  * circle
  */
 public class circle extends Shape2D {
-    double radius;
-    double area;
-    double perimeter;
+    private double radius;
+    private double area;
+    private double perimeter;
 
-    public circle() {
+    public circle(double radius) {
         super("Circle");
+        this.radius=radius;
     }
 
     public double getArea() {
         return area;
     }
-
+    public void CalcArea(double radius){
+        area=Math.PI*radius*radius;
+        this.area=area;
+    }
     public double getPerimeter() {
         return perimeter;
+    }
+    public void CalcPerimeter(double radius){
+        perimeter=Math.PI*radius;
+        this.perimeter=perimeter;
     }
 }
