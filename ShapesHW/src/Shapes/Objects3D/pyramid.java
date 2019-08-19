35631ed028c2dce1fca445b2ad57f33542942568
Alex Helo <thealexxd25@hotmail.com>
2019-08-19 08:17:36 -0500

    Updated Pyramid.java

--- ShapesHW/src/Shapes/Objects3D/pyramid.java
+++ ShapesHW/src/Shapes/Objects3D/pyramid.java
@@ -1,8 +1,30 @@
 package Shapes.Objects3D;
 
+import Shapes.Shape3D;
+
 /**
  * pyramid aka Tetraedro
  */
-public class pyramid {
+public class pyramid extends Shape3D {
+    double side;
+    double area;
+    double volume;
+    double height;
+    double perimeter;
 
+    public pyramid() {
+        super("Pyramid");
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
 }
