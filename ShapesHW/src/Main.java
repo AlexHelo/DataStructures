1d47c6a53542b586af5c0a8cd9d8b0e766e5bf67
Alex Helo <thealexxd25@hotmail.com>
2019-08-21 17:15:47 -0500

    minor changes

--- ShapesHW/src/Main.java
+++ ShapesHW/src/Main.java
@@ -1,3 +1,4 @@
+import Shapes.ShapeInterface;
 import java.util.Scanner;
 
 /**
@@ -5,8 +6,11 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        ShapeInterface sp = null;
+        while (sp = null) {
+            System.out.println("What type of shape do you want?" + "\n" + "1)2D Shape" + "\n" + "3D Shape");
+        }
 
     }
 }
