0409e7b8f255df91e5f37b4e09adf6a04aa6349c
Alex Helo <thealexxd25@hotmail.com>
2019-10-17 20:50:38 -0500

    created StudentID HW

---
+++ StudentUI/Student.java
@@ -0,0 +1,39 @@
+
+public class Student {
+
+    private String Name;
+    private String ID;
+    private float Grade;
+
+    public Student() {
+
+        this.Name = Name;
+        this.ID = ID;
+        this.Grade = Grade;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public float getGrade() {
+        return Grade;
+    }
+
+    public void setGrade(float grade) {
+        Grade = grade;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String iD) {
+        ID = iD;
+    }
+
+}
